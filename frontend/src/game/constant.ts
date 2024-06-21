1af67968a4e66a643383b779bbf3830a85c7af48
PritamDas <pritam.das.santu@gmail.com>
2024-06-21 22:51:06 +0530

    moved MULTIPLIERS constant to constant.ts

--- frontend/src/game/constant.ts
+++ frontend/src/game/constant.ts
@@ -11,3 +11,23 @@ export const verticalFriction = 0.8;
 
 export const sinkWidth = 36;
 export const NUM_SINKS = 17;
+
+export const MULTIPLIERS: { [key: number]: number } = {
+  1: 16,
+  2: 9,
+  3: 2,
+  4: 1.4,
+  5: 1.4,
+  6: 1.2,
+  7: 1.1,
+  8: 1,
+  9: 0.5,
+  10: 1,
+  11: 1.1,
+  12: 1.2,
+  13: 1.4,
+  14: 1.4,
+  15: 2,
+  16: 9,
+  17: 16,
+};
