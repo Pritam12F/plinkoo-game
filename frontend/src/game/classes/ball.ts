3e7640f45fcdefe82f20f60811c7a59ee87eff60
PritamDas <pritam.das.santu@gmail.com>
2024-06-22 11:26:47 +0530

    init ball class

---
+++ frontend/src/game/classes/ball.ts
@@ -0,0 +1,42 @@
+import { Obstacle, Sink } from "../objects";
+
+export class Ball {
+  private x: number;
+  private y: number;
+  private radius: number;
+  private color: string;
+  private vx: number;
+  private vy: number;
+  private ctx: CanvasRenderingContext2D;
+  private obstacles: Obstacle[];
+  private sinks: Sink[];
+  private onFinish: (index: number) => void;
+
+  constructor(
+    x: number,
+    y: number,
+    radius: number,
+    color: string,
+    vx: number,
+    vy: number,
+    ctx: CanvasRenderingContext2D,
+    obstacles: Obstacle[],
+    sinks: Sink[],
+    onFinish: (index: number) => void
+  ) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.vx = vx;
+    this.vy = vy;
+    this.ctx = ctx;
+    this.obstacles = obstacles;
+    this.sinks = sinks;
+    this.onFinish = onFinish;
+  }
+
+  draw() {}
+
+  update() {}
+}
