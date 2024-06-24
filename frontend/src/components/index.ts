475edb679bb13ad8d5b06597630afaea747ac4f5
PritamDas <pritam.das.santu@gmail.com>
2024-06-24 16:43:57 +0530

    added global exports to components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./Footer";
+export * from "./FoundIssue";
+export * from "./Game";
+export * from "./Navbar";
+export * from "./Quotes";
+export * from "./Simulate";
+export * from "./Simulation";
